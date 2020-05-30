e680a69cfdd2bd5e023a1d0f60484dd856084004
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-05-30 17:31:58 +0900

    plt.quiverの使用法ミスを修正

--- numerical_gradient.py
+++ numerical_gradient.py
@@ -7,7 +7,7 @@ def dual(x1,x2):
     出力：2変数多項式、係数、次数のリスト
     """
     #係数
-    a=[2,2]
+    a=[1,1]
     #次数
     k=[2,2]
     return a[0]*x1**k[0]+a[1]*x2**k[1],a,k
@@ -17,11 +17,11 @@ def gradient(f,x):
     入力：多項式関数、x(リスト)
     出力：各x(i,j)毎の全微分ベクトル(リスト)、多項式関数の係数、次数
     """
-    d=10e-4
+    d=10e-2
     grad_x1=np.zeros((len(x[0]),len(x[1])))
     grad_x2=np.zeros((len(x[0]),len(x[1])))
     a,k=f(0,0)[1:]
-    
+
     for i in range(len(x[0])):
         for j in range(len(x[1])):
             #x[0]成分の偏微分
@@ -50,13 +50,23 @@ def graph(X,Y,U,V,a,k):
 
 def main():
     #ベクトル表示倍率
-    mag=100000
+    mag=50
     #x1,x2の定義
     x=[np.arange(-10,10,1),np.arange(-10,10,1)]
     #数値微分
     U,V,a,k=gradient(dual,x)
-    #グラフプロット
-    graph(x[0],x[1],U*mag,V*mag,a,k)
+    x_=[]
+    y_=[]
+    u=[]
+    v=[]
+
+    for i in range(len(x[0])):
+        for j in range(len(x[1])):
+            x_.append(x[0][i])
+            y_.append(x[1][j])
+            u.append(U[i,j]*mag)
+            v.append(V[i,j]*mag)
+    graph(x_,y_,u,v,a,k)
     return
 
 if __name__=="__main__":
