ddf1bd4e7c4bd16af3dd5062044d4c2247ae20e3
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-05-30 13:14:26 +0900

    バッチ処理でトータル精度を求める関数を追加。バッチ数毎の処理時間計算機能を追加。バッチにより精度が異なる?

--- prediction_with_existing_dataset.py
+++ prediction_with_existing_dataset.py
@@ -3,6 +3,7 @@ import sys,os
 import matplotlib.pyplot as plt
 import pickle
 import matplotlib.pyplot as plt
+import time
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 #各数字の予測正答率をグラフ化
@@ -32,9 +33,33 @@ def predict(x,network):
     z1=sigmoid(np.dot(x,w1)+b1)
     z2=sigmoid(np.dot(z1,w2)+b2)
     z3=sigmoid(np.dot(z2,w3)+b3)
-    return np.argmax(z3)
+    return np.argmax(z3,axis=1)
+
+def cal_total_accuracy(batch_size):
+    """
+    0-9までの全数字のトータル予測精度の算出
+    入力：処理するバッチサイズ
+    出力：トータル予測精度(%)
+    """
+    x_test,t_test=get_data()
+    network=init_network()
+    N=len(x_test)
+    B=batch_size
+
+    accuracy_sum=0
+    for i in range(0,N-B,B):
+        x=x_test[i:i+B]
+        t=t_test[i:i+B]
+        accuracy=predict(x,network)
+        accuracy_sum+=np.sum(accuracy==t)
+    return accuracy_sum/N*100
 
 def cal_each_accuracy():
+    """
+    各数字の予測精度を算出。
+    入力：なし
+    出力：数字毎の予測精度(%)リスト
+    """
     x_test,t_test=get_data()
     network=init_network()
     N=len(x_test)
@@ -53,9 +78,34 @@ def graph_plot(x,y):
     plt.show()
 
 def main():
-    x=np.arange(0,10,1)
-    y=cal_each_accuracy()
-    graph_plot(x,y)
+    """
+    each_calがTrueの時個別の確率を計算し棒グラフを生成
+    Falseの時はトータル確率を表示
+    cal_timeがTrueの時は、バッチ数毎の処理時間グラフを生成
+    """
+    each_cal=False
+    cal_time=True
+
+    if each_cal==True:
+        x=np.arange(0,10,1)
+        y=cal_each_accuracy()
+        graph_plot(x,y)
+    else:
+        if cal_time==True:
+            batch_size=[1,2,5,10,100,200,500,1000,5000]
+            total_accuracy=[]
+            tact_time=[]
+            for e in batch_size:
+                time1=time.time()
+                total_accuracy.append(cal_total_accuracy(e))
+                time2=time.time()
+                tact_time.append(time2-time1)
+            #graph_plot(batch_size,tact_time)
+            print(tact_time)
+            print(total_accuracy)
+        else:
+            batch_size=100
+            print(cal_total_accuracy(batch_size))
 
 if __name__=="__main__":
     main()
